09963d145b9628e2190e43b08aa6819381e287c8
Benoit Pingris <benoit.pingris@icloud.com>
2025-09-25 16:52:01 +0200

    fix: merge all errors like string or object
    from the tests:
    ```ts
    const result = safeTry(async function* (_) {
    	yield* _(R.fail("error1" as const));
    	yield* _(R.fail("error2" as const));
    	yield* _(R.fail({ tag: "error3" as const }));
    	yield* _(await Promise.resolve(R.fail({ tag: "error4" as const })));
    
    	return R.succeed();
    });
    
    expectTypeOf(result).toEqualTypeOf<
    	R.ResultAsync<
    		void,
    		"error1" | "error2" | { tag: "error3" } | { tag: "error4" }
    	>
    ```

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,55 @@
-import { R } from '@praha/byethrow'
+import { R } from "@praha/byethrow";
 
 function adapter<T, E>(value: R.Result<T, E>) {
-  return {
-    ...value,
-    *[Symbol.iterator]() {
-      if (value.type === 'Success') {
-        return value.value
-      } else {
-        yield value
-        throw ''
-      }
-    }
-  }
+	return {
+		...value,
+		*[Symbol.iterator]() {
+			if (value.type === "Success") {
+				return value.value;
+			} else {
+				yield value;
+				throw "";
+			}
+		},
+	};
 }
 
-type Adapter = typeof adapter
+type Adapter = typeof adapter;
 
-export function safeTry<T, E>(body: (_: Adapter) => AsyncGenerator<R.Failure<E>, R.Result<T, E>>): R.ResultAsync<T, E>
-export function safeTry<T, E>(body: (_: Adapter) => Generator<R.Failure<E>, R.Result<T, E>>): R.Result<T, E>
-export function safeTry<T, E>(body: (_: Adapter) =>
-  | Generator<R.Failure<E>, R.Result<T, E>>
-  | AsyncGenerator<R.Failure<E>, R.Result<T, E>>
+export function safeTry<T, E>(
+	body: (_: Adapter) => AsyncGenerator<R.Failure<E>, R.Result<T, E>>,
+): R.ResultAsync<T, E>;
+export function safeTry<T, E>(
+	body: (_: Adapter) => Generator<R.Failure<E>, R.Result<T, E>>,
+): R.Result<T, E>;
+export function safeTry<
+	YieldFailure extends R.Failure<unknown>,
+	GeneratorReturnResult extends R.Result<unknown, unknown>,
+>(
+	body: (_: Adapter) => Generator<YieldFailure, GeneratorReturnResult>,
+): R.Result<
+	R.InferSuccess<GeneratorReturnResult>,
+	R.InferFailure<YieldFailure> | R.InferFailure<GeneratorReturnResult>
+>;
+export function safeTry<
+	YieldFailure extends R.Failure<unknown>,
+	GeneratorReturnResult extends R.Result<unknown, unknown>,
+>(
+	body: (_: Adapter) => AsyncGenerator<YieldFailure, GeneratorReturnResult>,
+): R.ResultAsync<
+	R.InferSuccess<GeneratorReturnResult>,
+	R.InferFailure<YieldFailure> | R.InferFailure<GeneratorReturnResult>
+>;
+export function safeTry<T, E>(
+	body: (
+		_: Adapter,
+	) =>
+		| Generator<R.Failure<E>, R.Result<T, E>>
+		| AsyncGenerator<R.Failure<E>, R.Result<T, E>>,
 ): R.Result<T, E> | R.ResultAsync<T, E> {
-  const n = body(adapter).next()
-  if (n instanceof Promise) {
-    return n.then(v => v.value)
-  }
-  return n.value
+	const n = body(adapter).next();
+	if (n instanceof Promise) {
+		return n.then((v) => v.value);
+	}
+	return n.value;
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,89 +1,109 @@
-import { describe, it, expect, mock } from 'bun:test'
-import { R } from '@praha/byethrow'
-import { safeTry } from '../src'
+import { describe, it, expect, mock, expectTypeOf } from "bun:test";
+import { R } from "@praha/byethrow";
+import { safeTry } from "../src";
 
-describe('safeTry', () => {
-  it('should return `succeed` value', () => {
-    const value = safeTry(function*(_) {
-      return R.succeed(1)
-    })
-    expect(R.isSuccess(value)).toBeTrue()
-    expect(R.unwrap(value)).toEqual(1)
-  })
+describe("safeTry", () => {
+	it("should return `succeed` value", () => {
+		const value = safeTry(function* (_) {
+			return R.succeed(1);
+		});
+		expect(R.isSuccess(value)).toBeTrue();
+		expect(R.unwrap(value)).toEqual(1);
+	});
 
-  it('should return `failure` value', () => {
-    const value = safeTry(function*(_) {
-      return R.fail('error' as const)
-    })
-    expect(R.isFailure(value)).toBeTrue()
-    expect(R.unwrapError(value)).toEqual('error')
-  })
+	it("should return `failure` value", () => {
+		const value = safeTry(function* (_) {
+			return R.fail("error" as const);
+		});
+		expect(R.isFailure(value)).toBeTrue();
+		expect(R.unwrapError(value)).toEqual("error");
+	});
 
-  it('should return `succeed` value in async function', async () => {
-    const value = await safeTry(async function*(_) {
-      return R.succeed(1)
-    })
-    expect(R.isSuccess(value)).toBeTrue()
-    expect(R.unwrap(value)).toEqual(1)
-  })
+	it("should return `succeed` value in async function", async () => {
+		const value = await safeTry(async function* (_) {
+			return R.succeed(1);
+		});
+		expect(R.isSuccess(value)).toBeTrue();
+		expect(R.unwrap(value)).toEqual(1);
+	});
 
-  it('should return `failure` value in async function', async () => {
-    const value = await safeTry(async function*(_) {
-      return R.fail('error' as const)
-    })
-    expect(R.isFailure(value)).toBeTrue()
-    expect(R.unwrapError(value)).toEqual('error')
-  })
+	it("should return `failure` value in async function", async () => {
+		const value = await safeTry(async function* (_) {
+			return R.fail("error" as const);
+		});
+		expect(R.isFailure(value)).toBeTrue();
+		expect(R.unwrapError(value)).toEqual("error");
+	});
 
-  it('should return `failure` value', () => {
-    const value = safeTry(function*(_) {
-      return R.fail('error' as const)
-    })
-    expect(R.isFailure(value)).toBeTrue()
-    expect(R.unwrapError(value)).toEqual('error')
-  })
+	it("should return `failure` value", () => {
+		const value = safeTry(function* (_) {
+			return R.fail("error" as const);
+		});
+		expect(R.isFailure(value)).toBeTrue();
+		expect(R.unwrapError(value)).toEqual("error");
+	});
 
-  it('should return error from safeTry body without calling next functions', () => {
-    const fn = mock()
-    const result = safeTry(function*(_) {
-      yield* _(R.fail('error'))
-      fn()
-      return R.succeed()
-    })
+	it("should return error from safeTry body without calling next functions", () => {
+		const fn = mock();
+		const result = safeTry(function* (_) {
+			yield* _(R.fail("error"));
+			fn();
+			return R.succeed();
+		});
 
-    expect(fn).not.toHaveBeenCalled()
-    expect(R.isFailure(result)).toBeTrue()
-    expect(result).toEqual(R.fail('error'))
-  })
+		expect(fn).not.toHaveBeenCalled();
+		expect(R.isFailure(result)).toBeTrue();
+		expect(result).toEqual(R.fail("error"));
+	});
 
-  it('should return error from safeTry async body without calling next functions', async () => {
-    const fn = mock()
-    const result = await safeTry(async function*(_) {
-      yield* _(R.fail('error'))
-      fn()
-      return R.succeed()
-    })
+	it("should return error from safeTry async body without calling next functions", async () => {
+		const fn = mock();
+		const result = await safeTry(async function* (_) {
+			yield* _(R.fail("error" as const));
+			fn();
+			return R.succeed();
+		});
 
-    expect(fn).not.toHaveBeenCalled()
-    expect(R.isFailure(result)).toBeTrue()
-    expect(result).toEqual(R.fail('error'))
-  })
+		expect(fn).not.toHaveBeenCalled();
+		expect(R.isFailure(result)).toBeTrue();
+		expect(result).toEqual(R.fail("error"));
+	});
 
-  it('should work with sync and async operations in the same body', async () => {
-    const succeedValues: number[] = []
+	it("should work with sync and async operations in the same body", async () => {
+		const succeedValues: number[] = [];
 
-    const result = await safeTry(async function*(_) {
-      const a = yield* _(R.succeed(1))
-      succeedValues.push(a)
-      const b = yield* _(await R.succeed(Promise.resolve(2)))
-      succeedValues.push(b)
-      const c = yield* _(R.succeed(3))
-      succeedValues.push(c)
+		const result = await safeTry(async function* (_) {
+			const a = yield* _(R.succeed(1));
+			succeedValues.push(a);
+			const b = yield* _(await R.succeed(Promise.resolve(2)));
+			succeedValues.push(b);
+			const c = yield* _(R.succeed(3));
+			succeedValues.push(c);
 
-      return R.succeed()
-    })
+			return R.succeed();
+		});
 
-    expect(R.isSuccess(result)).toBeTrue()
-    expect(succeedValues).toEqual([1, 2, 3])
-  })
-})
+		expect(R.isSuccess(result)).toBeTrue();
+		expect(succeedValues).toEqual([1, 2, 3]);
+	});
+
+	it("should merge all errors", async () => {
+		const result = safeTry(async function* (_) {
+			yield* _(R.fail("error1" as const));
+			yield* _(R.fail("error2" as const));
+			yield* _(R.fail({ tag: "error3" as const }));
+			yield* _(await Promise.resolve(R.fail({ tag: "error4" as const })));
+
+			return R.succeed();
+		});
+
+		expectTypeOf(result).toEqualTypeOf<
+			R.ResultAsync<
+				void,
+				"error1" | "error2" | { tag: "error3" } | { tag: "error4" }
+			>
+		>();
+
+		expect(R.isFailure(await result)).toBeTrue();
+	});
+});
